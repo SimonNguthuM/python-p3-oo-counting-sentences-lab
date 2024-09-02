eb0443d93813f301f9da301d160f9b1b5d170998
Simon Nguthu <simon.nguthu@student.moringaschool.com>
2024-09-02 18:15:30 +0300

    Counting Sentences

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    def __init__(self, value=""):
+        self._value = value
+
+    @property
+    def value(self):
+        return self._value
+    
+    @value.setter
+    def value(self, value):
+        """The value must be a string."""
+        if isinstance(value, str):
+            self._value = value
+        else:
+            print("The value must be a string.")
+      
+    def is_sentence(self):
+        return self.value.endswith('.')
+        
+    def is_question(self):
+        return self.value.endswith('?')
+        
+    def is_exclamation(self):
+        return self.value.endswith('!')
+        
+    def count_sentences(self):
+        standardized_value = self.value.replace('!', '.').replace('?', '.').split('.')
+        sentences = [sentence.strip() for sentence in standardized_value if sentence]
+        return len(sentences)  
+    
+
